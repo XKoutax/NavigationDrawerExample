5ed78ad29b1de63be4d88c4121d039b696764d80
XKoutax <carolsfartz@gmail.com>
2019-03-05 19:00:41 +0200

    Initial commit

---
+++ app/src/main/java/com/bcs/andy/navigationdrawerexample/MainActivity.java
@@ -0,0 +1,44 @@
+package com.bcs.andy.navigationdrawerexample;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // ---------------- we use this bit of code in order to have BUTTON in the top left corner, which openes the drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // we write this "if..." to close the drawer, that is on the left side of the screen
+        // if the drawer was on the right side, we would've used GravityCompat.END
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
