60030867abf9780e6e0f6587a0b7b89dd7506ae0
XKoutax <carolsfartz@gmail.com>
2019-03-11 13:27:08 +0200

    added onClickListener and Fragments for NavigationDrawer buttons, separated toolbar in it's own layout

--- app/src/main/java/com/bcs/andy/navigationdrawerexample/MainActivity.java
+++ app/src/main/java/com/bcs/andy/navigationdrawerexample/MainActivity.java
@@ -1,33 +1,65 @@
 package com.bcs.andy.navigationdrawerexample;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.myToolbar);
         setSupportActionBar(toolbar);
 
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (DrawerLayout) findViewById(R.id.myDrawer_layout);
 
-        // ---------------- we use this bit of code in order to have BUTTON in the top left corner, which openes the drawer
+
+        //  we use this bit of code in order to have BUTTON in the top left corner, which openes the drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
 
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        // we can to the bellow thing, or
+        // we can implement NavigationView.OnNavigationItemSelectedListener   in our Activity
+        // and override the onNavigationItemSelected method
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                return false;
+//            }
+//        });
+
+
+
+        // upon rotating the device, the activity gets destroyed and re-created
+        // so the problem is that upon rotating, it will reset to the below fragment. we solve it with this below IF
+        // savedInstanceState == null  ONLY  when we start the activity the FirstTime, or when we leave it with BACK button and then get back into it
+        // if we rotate the devile, savedInstanceState != null
+        if (savedInstanceState == null) {
+            // this will open our MessageFragment upon creation of the activity, before clicking anything
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_message);
+        }
 
     }
 
@@ -35,10 +67,37 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         // we write this "if..." to close the drawer, that is on the left side of the screen
         // if the drawer was on the right side, we would've used GravityCompat.END
-        if(drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            // means drawer is not opened
             super.onBackPressed();
         }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_message:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+                startActivity(new Intent(this, BbbActivity.class));
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;    // return false = no item will be selected, even though the action was triggered
     }
 }
